28c738e0d4209a1e04ef00f9e417bffff165c4ce
Roman <suhanovroman96@yandex.ru>
2019-09-16 18:43:33 +0300

    Start_lab2_vector

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <QCoreApplication>
+
+#include <vector>
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    
+    
+    
+    return 0; // a.exec();
+}
